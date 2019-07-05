c22ebd4eda7ba540e8c7ebf766593592458e34e6
Fumiya <fumiya.89.1216@email.com>
2019-07-05 15:42:16 +0000

    Finish edit-attendances

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#top'
   get  '/signup', to: 'users#new'
-  resources :users
+  
   # ログイン機能
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     member do
       get 'edit_basic_info'
       patch 'update_basic_info'
+      get 'attendances/edit_one_month'
+      patch 'attendances/update_one_month'
     end
     resources :attendances, only: :update
   end
